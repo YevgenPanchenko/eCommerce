6f7e88305b99bd5a6fdc00641f00b67627ce238a
YevgenPanchenko <jeremyblend@gmail.com>
2023-08-02 23:40:03 +0200

    Add Footer and Subscribe components

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           sx={{ "&:hover": { cursor: "pointer" } }}
           color={shades.secondary[500]}
         >
-          eCommerce
+          e.Commerce
         </Box>
         <Box
           display="flex"
